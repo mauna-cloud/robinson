4140b4cce38c1f8a1ae51809280c4f38c3c3cf8d
Dmitry <dmitry@mauna.ai>
2020-08-27 22:17:44 +0300

    Perform refactoring

--- src/ssml.rs
+++ src/ssml.rs
@@ -1,50 +1,52 @@
-use style::{StyledNode};
-use dom::{NodeType, ElementData};
-use css::{Value};
+use css::Value;
+use dom::{ElementData, NodeType};
+use std::collections::HashMap;
+use style::StyledNode;
 
-fn parse_spec_val(val: &Value) -> String {
+fn render_spec_val(val: &Value) -> String {
     return match val {
         Value::Keyword(s) => s.to_string(),
         Value::ColorValue(c) => format!("#{:02X}{:02X}{:02X}", c.r, c.g, c.b),
-        Value::Length(l, _) => format!("{}px", l) 
+        Value::Length(l, _) => format!("{}px", l),
+    };
+}
+
+fn render_spec_vals(vals: HashMap<String, Value>) -> String {
+    let mut result = String::from("");
+    for (spec_key, spec_val) in vals {
+        result = format!(
+            " {}=\"{}\"",
+            spec_key.as_str(),
+            render_spec_val(&spec_val).as_str()
+        );
     }
+    return result;
 }
 
 pub fn render_ssml(styled_node: StyledNode, level: usize) -> String {
     let spec_vals = styled_node.specified_values;
     let indent = " ".repeat(level * 2);
-    let mut result = String::from("");
     match &styled_node.node.node_type {
         NodeType::Text(s) => {
-            result.push_str(indent.as_str());
-            result.push_str(s.trim());
-            result.push_str("\n");
-            return result;
-        },
-        NodeType::Element(ElementData {tag_name: tag, attributes: _}) => {
-            // open tag
-            result.push_str(indent.as_str());
-            result.push_str("<");
-            result.push_str(tag);
-            //TODO: move above in order to be able to wrap into tags 
-            for (spec_key, spec_val) in spec_vals {
-                result.push_str(" ");
-                result.push_str(spec_key.as_str());
-                result.push_str("=\"");
-                result.push_str(parse_spec_val(&spec_val).as_str());
-                result.push_str("\"");
-            }
-            result.push_str(">\n");
-            // children
+            return format!("{}{}\n", indent.as_str(), s.trim());
+        }
+        NodeType::Element(ElementData {
+            tag_name: tag,
+            attributes: _,
+        }) => {
+            let mut children_str = String::from("");
             for child in styled_node.children {
-                result.push_str(render_ssml(child, level + 1).as_str());
+                children_str.push_str(render_ssml(child, level + 1).as_str());
             }
-            // close tag
-            result.push_str(indent.as_str());
-            result.push_str("</");
-            result.push_str(tag);
-            result.push_str(">\n");
-            return result;
-        }, 
+            return format!(
+                "{}<{}{}>\n{}{}</{}>\n",
+                indent.as_str(),
+                tag,
+                render_spec_vals(spec_vals),
+                children_str,
+                indent.as_str(),
+                tag
+            );
+        }
     };
 }
