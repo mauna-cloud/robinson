2a2ddd621bbffc7208ac236cd29f8479b092a1d5
Dmitry <dmitry@mauna.ai>
2020-08-28 07:31:22 +0300

    Perform refactoring, implement Display trait for attribute values

--- src/ssml.rs
+++ src/ssml.rs
@@ -2,30 +2,29 @@ use css::Value;
 use dom::{ElementData, NodeType};
 use std::collections::HashMap;
 use style::StyledNode;
+use std::fmt::{Display, Formatter, Result};
 
-fn render_spec_val(val: &Value) -> String {
-    return match val {
-        Value::Keyword(s) => s.to_string(),
-        Value::ColorValue(c) => format!("#{:02X}{:02X}{:02X}", c.r, c.g, c.b),
-        Value::Length(l, _) => format!("{}px", l),
-    };
-}
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        return match self {
+            Value::Keyword(s) => write!(f, "{}", s),
+            Value::ColorValue(c) => write!(f, "#{:02X}{:02X}{:02X}", c.r, c.g, c.b),
+            Value::Length(l, _) => write!(f, "{}px", l),
+        };
+    }
+} 
 
-fn render_spec_vals(vals: HashMap<String, Value>) -> String {
-    let mut result = String::from("");
-    for (spec_key, spec_val) in vals {
-        let props = format!(
-            " {}=\"{}\"",
-            spec_key.as_str(),
-            render_spec_val(&spec_val).as_str()
-        );
-        result.push_str(props.as_str());
+fn render_attrs<T: Display>(attrs: &HashMap<String, T>) -> String {
+    let mut result = String::new();
+    for (k, v) in attrs {
+        let attr_str = format!(" {}=\"{}\"", k, v);
+        result.push_str(attr_str.as_str());
     }
     return result;
 }
 
 pub fn render_ssml(styled_node: StyledNode, level: usize) -> String {
-    let spec_vals = styled_node.specified_values;
+    let spec_vals = &styled_node.specified_values;
     let indent = " ".repeat(level * 2);
     match &styled_node.node.node_type {
         NodeType::Text(s) => {
@@ -33,17 +32,18 @@ pub fn render_ssml(styled_node: StyledNode, level: usize) -> String {
         }
         NodeType::Element(ElementData {
             tag_name: tag,
-            attributes: _,
+            attributes: attrs,
         }) => {
             let mut children_str = String::from("");
             for child in styled_node.children {
                 children_str.push_str(render_ssml(child, level + 1).as_str());
             }
             return format!(
-                "{}<{}{}>\n{}{}</{}>\n",
+                "{}<{}{}{}>\n{}{}</{}>\n",
                 indent.as_str(),
                 tag,
-                render_spec_vals(spec_vals),
+                render_attrs(attrs),
+                render_attrs(spec_vals),
                 children_str,
                 indent.as_str(),
                 tag
